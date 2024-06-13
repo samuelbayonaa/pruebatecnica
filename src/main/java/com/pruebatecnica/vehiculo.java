9ce37e1bb57524d55e71ea0371862c6c83cea236
Samuel Bayona <159915786+samuelbayonaa@users.noreply.github.com>
2024-06-13 17:38:16 -0500

    Update vehiculo.java

--- src/main/java/com/pruebatecnica/vehiculo.java
+++ src/main/java/com/pruebatecnica/vehiculo.java
@@ -1,5 +1,40 @@
 package com.pruebatecnica;
 
-public class Vehiculo {
+public class vehiculo {
 
+    Integer matricula;
+    String modelo;
+    Integer potenciaCv;
+    
+    public vehiculo(Integer matricula, String modelo, Integer potenciaCv) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.potenciaCv = potenciaCv;
+    }
+
+    public Integer getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Integer matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Integer getPotenciaCv() {
+        return potenciaCv;
+    }
+
+    public void setPotenciaCv(Integer potenciaCv) {
+        this.potenciaCv = potenciaCv;
+    }
+
+    
 }
