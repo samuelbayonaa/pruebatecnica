99c87797f31b22e81ed055e1240bbf0c0949eca6
Samuel Bayona <159915786+samuelbayonaa@users.noreply.github.com>
2024-06-13 17:41:05 -0500

    Update Main.java

--- src/main/java/com/pruebatecnica/Main.java
+++ src/main/java/com/pruebatecnica/Main.java
@@ -1,5 +1,5 @@
 package com.pruebatecnica;
 
 public class Main {
-
+//De antemano una disculpa por subir el contenido en ese momento, no quiso guardar ya que tuve un problema con una cuenta en el portatil, gracias.
 }
